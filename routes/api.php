131ee38a2092a6b0c35239435d2af1b4d30f1c38
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-02 16:05:55 +0300

    Added laravel/ui, auth and bootstrap

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'User', 'prefix' => 'user', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', 'ShowController');
+});
