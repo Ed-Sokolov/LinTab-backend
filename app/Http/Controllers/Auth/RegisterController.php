fee9ec14ab043b9db8ef4851c2c3f230b7b28fc8
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-05 12:59:51 +0300

    Changed length for user nickname in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'nickname' => ['required', 'string', 'max:255', 'unique:users'],
+            'nickname' => ['required', 'string', 'max:50', 'min:4', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
