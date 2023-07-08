c62d2f5e28cddf680bb7becd9a71fa5f0d9b49ab
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-08 17:04:48 +0300

    Updated Post/UpdateController and Post/DestroyController

--- app/Http/Controllers/Post/DestroyController.php
+++ app/Http/Controllers/Post/DestroyController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class DestroyController extends Controller
 {
     public function __invoke(Post $post)
     {
-        if ($post->author_id !== auth()->id) {
+        if ($post->author_id !== Auth::id()) {
             return response()->json([
                 'message' => 'You are not the author of this post'
             ], Response::HTTP_FORBIDDEN);
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -6,12 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\UpdateRequest;
 use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateController extends Controller
 {
     public function __invoke(Post $post, UpdateRequest $request)
     {
         $data = $request->validated();
+
+        if ($post->author_id !== Auth::id()) {
+            return response()->json([
+                'message' => 'You are not the author of this post'
+            ], Response::HTTP_FORBIDDEN);
+        }
 
         $post->update($data);
 
