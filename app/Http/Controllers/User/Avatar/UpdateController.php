5eefe84f589df18081c42145774ea41ac4b7f248
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-16 10:40:13 +0300

    Removed and added a new column to users table

---
+++ app/Http/Controllers/User/Avatar/UpdateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\User\Avatar;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
