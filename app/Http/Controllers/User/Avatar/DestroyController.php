d034fcfaa9558e3737b3960444ea5b321dbe9ecd
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-20 22:11:56 +0300

    Modified Avatar/DestroyController

--- app/Http/Controllers/User/Avatar/DestroyController.php
+++ app/Http/Controllers/User/Avatar/DestroyController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\User\Avatar;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\User\UserResource;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class DestroyController extends Controller
 {
     public function __invoke()
     {
+        $user = Auth::user();
 
+        $currentAvatar = $user->avatar;
+
+        if (isset($currentAvatar)) {
+            Storage::disk('public')->delete($currentAvatar->path);
+            Storage::disk('public')->delete(str_replace('images/avatars/', 'images/avatars/prev_', $currentAvatar->path));
+            $currentAvatar->delete();
+        }
+
+        $user->refresh();
+
+        return new UserResource($user);
     }
 }
