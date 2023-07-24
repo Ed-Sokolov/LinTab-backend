767181319a756963817adf88144556138fad45b3
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-24 22:47:05 +0300

    Created filter for posts

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Http\Resources\Post\PostCardResource;
 use App\Models\Post;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::with('views')->get();
+        $data = $request->validated();
+
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+
+        $posts = Post::filter($filter)->with('views')->get();
 
         return PostCardResource::collection($posts);
     }
